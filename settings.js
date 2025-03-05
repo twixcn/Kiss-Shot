f506a574a30309d70a6675dee469dc4c8fb0c6f9
twixcn <conancn2005@gmail.com>
2025-03-05 21:45:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Kiss Shoot',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Kiss Shot Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '249993957931', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
